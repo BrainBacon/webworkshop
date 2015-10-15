fafc20c12c67e24138bb900e453681895b67584a
Brian Jesse <brian.jesse@gmail.com>
2015-10-15 13:39:08 -0500

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+Flatdoc.run({
+    fetcher: Flatdoc.github('BrainBacon/webworkshop')
+});
+
+/*
+$(document).on('flatdoc:ready', function() {
+});
+/*
+
